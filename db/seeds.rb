b512045ca0a1a4d3c1272e382e5bae1ca4e1eaaa
lakshmanan <lakshmanan.murugan@mallow-tech.com>
2023-09-29 12:21:40 +0530

    latest commited

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 (1..4).each do |seed|
-  Entrance.create(way:"Entrance #{seed}")
+  Entrance.create(way:"entrance #{seed}")
 end
 
-slot_no = 0
-(1..4).each do |seeds|
+
+slot_number = 0
+(1..4).each do |entry|
   (1..10).each do |column|
     (1..25).each do |row|
-      slot_no += 1
-      Slot.create(slot_number: slot_no, slot_available: false , slot_distance: row + column, entrance_id: seeds)
+      slot_number += 1
+      Slot.create(slot_number: slot_number, slot_available: false, slot_distance:  row + column,entrance_id: entry)
     end
   end
 end
